e83ecbe86462e40b672024c9ce0ecebdc8f21151
yabecchi312 <yabehideaki@MacBook-Pro-4.local>
2018-12-16 15:33:22 +0900

    user controller create
    userコントローラーにてCRUDの作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,8 @@
 class Admin::UsersController < ApplicationController
+  def index
+    @user = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -13,13 +17,28 @@ class Admin::UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update
+      redirect_to admin_users_path, notice: "ユーザー「#{@user.name}」を登録しました。"
+    else
+      render :new
+    end
+  end
+
   def edit
+    @user = User.find(params[:id])
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
-  def index
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザー「#{@user.name}」を削除しました。"
   end
 
   private
