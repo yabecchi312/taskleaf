50453bef31a6236a4fd2604f4a8d5bacd8b8cfaf
yabecchi312 <yabehideaki@MacBook-Pro-4.local>
2018-12-16 20:58:41 +0900

    tasks_spec common loginuser
    ログインユーザーのコードの共通化

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,24 +2,28 @@ require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
+    #ユーザーAを作成しておく
     let(:user_a) {FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')}
     let(:user_b) {FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com')}
 
     before do
-      #ユーザーAを作成しておく
-      user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
-      #作成者がユーザーAであるタスクを作成しておく
+      #作成者がユーザーであるタスクを作成しておく
       FactoryBot.create(:task, name: '最初のタスク', user: user_a)
+      visit login_path
+      fill_in 'メールアドレス', with: login_user.email
+      fill_in 'パスワード', with: login_user.password
+      click_button 'ログインする'
     end
 
     context 'ユーザーAがログインしているとき' do
-      before do
-        #ユーザーがログインする
-        visit login_path
-        fill_in 'メールアドレス', with: 'a@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログインする'
-      end
+      # before do
+      #   #ユーザーがログインする
+      #   # visit login_path
+      #   # fill_in 'メールアドレス', with: 'a@example.com'
+      #   # fill_in 'パスワード', with: 'password'
+      #   # click_button 'ログインする'
+      # end
+      let(:login_user) {user_a}
 
       it 'ユーザーAが作成したタスクが表示される' do
         # 作成済みのタスクの名称が画面上に表示されていることを確認
@@ -28,15 +32,17 @@ describe 'タスク管理機能', type: :system do
     end
 
     context 'ユーザーBがログインしているとき' do
-      before do
-        # ユーザーBを作成しておく
-        FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com')
-        # ユーザーBでログインをする
-        visit login_path
-        fill_in 'メールアドレス', with: 'b@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログインする'
-      end
+      # before do
+      #   # ユーザーBを作成しておく
+      #   FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com')
+      #   # ユーザーBでログインをする
+      #   visit login_path
+      #   fill_in 'メールアドレス', with: 'b@example.com'
+      #   fill_in 'パスワード', with: 'password'
+      #   click_button 'ログインする'
+      # end
+      let(:login_user) {user_b}
+
 
       it 'ユーザーAが作成したタスクが表示されない' do
         # ユーザーAが作成したタスクの名称が画面上に表示されていないことを確認
