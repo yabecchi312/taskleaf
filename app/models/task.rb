84afad83e4c5a5e2702ba881e6ce074f2032b50a
yabecchi312 <yabehideaki@MacBook-Pro-4.local>
2018-12-16 23:24:41 +0900

    ransack  install
    名称、作成日時の検索機能を追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,6 +7,14 @@ class Task < ApplicationRecord
   scope :recent, -> {order(created_at: :desc)}
   #繰り返し利用される絞り込み条件を読みやすくする。recentというメソッドを用意した
 
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name created_at]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
+
   private
 
   def validate_name_not_including_comma
