61bf9e786145622aaedafcb9b13360124cebfcf8
yabecchi312 <yabehideaki@MacBook-Pro-4.local>
2018-12-16 21:24:33 +0900

    shared_examples_for
    共通化する

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -15,6 +15,10 @@ describe 'タスク管理機能', type: :system do
     click_button 'ログインする'
   end
 
+  shared_examples_for 'ユーザーAが作成したタスクが表示される' do
+    it {expect(page).to have_content '最初のタスク'}
+  end
+
   describe '一覧表示機能' do
     context 'ユーザーAがログインしているとき' do
       # before do
@@ -26,10 +30,12 @@ describe 'タスク管理機能', type: :system do
       # end
       let(:login_user) {user_a}
 
-      it 'ユーザーAが作成したタスクが表示される' do
-        # 作成済みのタスクの名称が画面上に表示されていることを確認
-        expect(page).to have_content '最初のタスク'
-      end
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
+      # shared_examples_forに共通化して書く
+      # it 'ユーザーAが作成したタスクが表示される' do
+      #   # 作成済みのタスクの名称が画面上に表示されていることを確認
+      #   expect(page).to have_content '最初のタスク'
+      # end
     end
 
     context 'ユーザーBがログインしているとき' do
@@ -60,9 +66,7 @@ describe 'タスク管理機能', type: :system do
         visit task_path(task_a)
       end
 
-      it 'ユーザーAが作成したタスクが表示される' do
-        expect(page).to have_content '最初のタスク'
-      end
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
     end
   end
 end
